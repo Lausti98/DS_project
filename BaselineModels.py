be8c13060e58c7d62b76838234b0d3afba49a2ed
AndersSteiness <101812404+AndersSteiness@users.noreply.github.com>
2022-05-19 14:38:06 +0200

    Update BaselineModels.py

--- BaselineModels.py
+++ BaselineModels.py
@@ -1,4 +1,19 @@
-import numpy
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+import psycopg2 
 
-print("WUHU"
-)
+conn = psycopg2.connect("host=localhost dbname=moviedb user=XXX password=XXX")
+cur = conn.cursor()
+
+
+
+
+
+
+
