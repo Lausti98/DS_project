b9ec220a778d05539dba2fa5620a82b89286dbfd
AndersSteiness <101812404+AndersSteiness@users.noreply.github.com>
2022-05-31 19:52:41 +0200

    Update KeyPropertiesOfDataset.py

--- KeyPropertiesOfDataset.py
+++ KeyPropertiesOfDataset.py
@@ -10,7 +10,7 @@ cur = conn.cursor()
 # A query of all articles with all entities joined
 try:
     cur.execute("""
-    select type, count(*)
+    select type, count(DISTINCT content)
 	    from article
         inner join has_type ht on article.id = ht.article_id
         inner join type t on ht.type_id = t.id
@@ -21,10 +21,35 @@ except:
     print("rolled back")
     conn.rollback()
 
+dict_Entire_Fakenews = {'fake': 928083/9408908, 
+                        'satire': 146080/9408908,
+                        'bias': 1300444/9408908,
+                        'conspiracy': 905981/9408908,
+                        'junksci': 144939/9408908,
+                        'hate': 117374/9408908,
+                        'clickbait': 292201/9408908,
+                        'unreliable': 319830/9408908,
+                        'reliable': 1920139/9408908,
+                        'political': 2435471/9408908,
+                        }
+df_fakeNewsCorpus = pd.DataFrame(dict_Entire_Fakenews.items(), columns=['Types', 'Entire FakeNewsCorpus'])
+
+
 df = pd.DataFrame(cur.fetchall(), columns=['Types', 'Count'])
+Total = df['Count'].sum()
+df['Our 100000 sample'] = df['Count']/Total
+df= df.join(df_fakeNewsCorpus.set_index('Types'), on='Types')
 
 # plotting a bar graph
-df.plot(x="Types", y="Count", kind="bar")
+labels = ['Our sub sample', 'Entire FakeNewsCorpus']
+
+#plt.hist(df['Types'],df[['Proportion', 'Proportion FakeNewsCorpus']], 12, density=True, histtype='bar', color=['blue', 'yellow'], label=labels)
+#plt.legend(prop={'size': 10})
+#plt.set_title('bars with legend')
+#plt.show()
+
+
+df.plot(x="Types", y=["Our 100000 sample", "Entire FakeNewsCorpus"], kind="bar", xlabel = 'Type', ylabel = 'Frequency', width=0.6)
 plt.show()
 
 
